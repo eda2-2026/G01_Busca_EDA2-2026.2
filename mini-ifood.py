eec65dc60a2ba3136838375a7ff8433f2be6c9ef
ellevyn7 <evellynrocha8524@gmail.com>
2026-08-27 14:44:57 -0300

    feat: adiciona dados de restaurantes e entregadores

--- mini-ifood.py
+++ mini-ifood.py
@@ -13,3 +13,19 @@ class Entregador:
         self.x = x
         self.y = y
 
+restaurantes = [
+    Restaurante(1, "Pina hamburguers", 2, 5),
+    Restaurante(2, "Boa comida", 8, 3),
+    Restaurante(3, "Nonna Ristorante", 4, 9),
+    Restaurante(4, "Mia madre italiana", 1, 2),
+    Restaurante(5, "Cantina da massa", 7, 7)
+]
+
+
+entregadores = [
+    Entregador(1, "Jean", True, 3, 4),
+    Entregador(2, "Maria", False, 9, 2),
+    Entregador(3, "Paulo", True, 5, 8),
+    Entregador(4, "Ana beatriz", True, 1, 6)
+]        
+
